5ee9702363f73f40c2627876533674763ea99608
RayG-XD <mail.pratikpol@gmail.com>
2024-04-25 11:08:29 +0530

    changes in config.yml

--- src/features/auth/controllers/test/signup.test.ts
+++ src/features/auth/controllers/test/signup.test.ts
@@ -152,7 +152,7 @@ describe('SignUp', () => {
       {
         username: 'Manny',
         email: 'manny@test.com',
-        password: 'mathematics123456789876436534634',
+        password: 'mathematics1',
         avatarColor: 'red',
         avatarImage: 'data:text/plain;base64,SGVsbG8sIFdvcmxkIQ=='
       }
